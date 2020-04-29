d411c008af213042d82003df0ac8b770185bc3bd
chris-baby <918128078@qq.com>
2020-04-29 11:01:26 +0800

    分页

--- web/controllers/account/Account.py
+++ web/controllers/account/Account.py
@@ -1,7 +1,7 @@
 from flask import Blueprint,request,redirect,jsonify
 from sqlalchemy import or_
 
-from common.libs.Helper import ops_render,getCurrentDate
+from common.libs.Helper import ops_render,getCurrentDate,iPagenation
 from common.libs.UrlManager import UrlManager
 from common.libs.user.UserService import UserService
 from common.models.User import User
@@ -14,15 +14,31 @@ def index():
     resp_data = {}
     query = User.query
     req = request.values
+    page = int(req['p']) if ('p' in req and req['p']) else 1
     if 'status' in req and int(req['status']) > -1:
         query = query.filter(User.status == int(req['status']))
     if 'mix_kw' in req:
         rule = or_( User.nickname.ilike("%{0}%".format(req['mix_kw'])), User.mobile.ilike("%{0}%".format(req['mix_kw'])) )
         query = query.filter(rule)
 
-    list = query.all()
+    params = {
+        "total":query.count(),
+        "page_size":2,
+        "page":page,
+        "url":request.full_path.replace("&p={}".format(page),"")
+    }
+    pages = iPagenation(params)
+    # 当前页数据开始位置  
+    offset = (page-1) * 2
+    # 当前页数据结束位置  
+    limit = page * 2
+
+    list = query.all()[offset:limit]
+
+
     resp_data['list'] = list
     resp_data['status'] = app.config['STATUS']
+    resp_data['pages'] = pages
     return ops_render("/account/index.html",resp_data)
 
 @router_account.route("/info")
